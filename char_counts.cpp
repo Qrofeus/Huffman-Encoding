9b8877d4d1e4705946789bb6693914521e4d3025
Qrofeus <ninad9021@gmail.com>
2023-11-16 19:50:56 -0500

    Part 1: Complete

---
+++ char_counts.cpp
@@ -0,0 +1,59 @@
+#include "char_counts.h"
+
+using namespace std;
+
+void ChCounts::generate_character_counts(string file_name) {
+	ifstream file_pointer;
+	file_pointer.open(file_name);
+
+	if (!file_pointer.is_open()) {
+		cout << "File could not be opened.";
+		file_pointer.close();
+		exit(1);
+	}
+
+	this->character_counts.assign(ASCII_CHARACTERS, 0);
+	char ch;
+
+	while (file_pointer.get(ch)) {
+		if (ch == EOF)
+			continue;
+
+		this->character_counts[(int)ch] += 1;
+	}
+
+	file_pointer.close();
+}
+
+void ChCounts::write_to_file(string file_name) {
+	ofstream file_pointer;
+	file_pointer.open(file_name);
+
+	if (!file_pointer.is_open()) {
+		cout << "File could not be opened.";
+		file_pointer.close();
+		exit(1);
+	}
+
+	for (int i = 0; i < ASCII_CHARACTERS; i++) {
+		//Non-printable line-feed character '\n' as 'LF'
+		if (i == 10) {
+			file_pointer << "LF " << this->character_counts[i] << endl;
+			continue;
+		}
+
+		//Printable ASCII Characters start from index 32 -> " "
+		//https://en.cppreference.com/w/cpp/language/ascii
+		if (i < 32)
+			continue;
+
+		//Ignore characters with count == 0
+		if (this->character_counts[i] == 0)
+			continue;
+
+		file_pointer << char(i) << ' ' << this->character_counts[i] << endl;
+	}
+
+	file_pointer.close();
+	cout << "Character counts written to file: " << file_name << endl;
+}
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "utility.h"
+#include "char_counts.h"
+
+using namespace std;
+
+//Team 6: Dasari, Deshpande, Kulkarni S., Nidumukkala, Pandit, Phirke
+
+//---
+const string READ_FILE_1 = "clear.txt";
+const string WRITE_FILE_1 = "freq.txt";
+//---
+
+int main() {
+	//Part 1:
+	//Generate character count from given text file
+	ChCounts ch_counter;
+	ch_counter.generate_character_counts(READ_FILE_1);
+	ch_counter.write_to_file(WRITE_FILE_1);
+
+	return 0;
+}
